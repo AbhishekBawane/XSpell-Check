c289beec5f104e2ee593520bb288abf5fc23d207
Abhishek Prakash Bawane <120009589+AbhishekBawane@users.noreply.github.com>
2025-11-15 21:04:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react"
+
+const customDictionary = {
+
+  teh: "the",
+
+  wrok: "work",
+
+  fot: "for",
+
+  exampl: "example"
+
+};
+
 
 function App() {
+
+  const [text, setText] = useState("");
+  const [suggestion, setSuggestion] = useState("");
+
+  function handleChange(e) {
+    const input = e.target.value;
+    setText(input);
+
+    if (input.trim() === "") {
+      setSuggestion("");
+      return;
+    }
+
+    const words = input.split(/\s+/);
+
+    for (let word of words) {
+      const lower = word.toLowerCase();
+
+      if (customDictionary[lower]) {
+        setSuggestion(`${customDictionary[lower]}?`);
+        return;
+      }
+    }
+    setSuggestion("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>Spell Check and Auto-Correct</h1>
+      <textarea
+        onChange={handleChange}
+        value={text}
+        placeholder="Enter text"
+        rows="6"
+        style={{
+          width: "600px",
+          padding: "10px",
+          fontSize: "16px",
+          borderRadius: "8px",
+          border: "1px solid #ccc",
+        }}
+        ></textarea>
+        {suggestion && (
+        <p style={{ marginTop: "15px", fontSize: "18px", }}>
+          Did you mean: {suggestion}?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
